2e01a00ac8c6cb5e0c258d75107f7c50cbe6148d
Felix Ribeiro Mansor Junior <felix.mansor@superdigital.com.br>
2017-11-14 17:42:27 -0200

    Versionando a rota comentada

--- gerenciador-de-tarefas/src/app/app-routing.module.ts
+++ gerenciador-de-tarefas/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 // import { TarefaRoutes } from './tarefas';
 
 export const routes: Routes = [
-	/*{ 
-		path: '', 
-		redirectTo: '/tarefas/listar', 
-		pathMatch: 'full' 
-	},*/
+	// { 
+	// 	path: '', 
+	// 	redirectTo: '/tarefas/listar', 
+	// 	pathMatch: 'full' 
+	// },
 	// ...TarefaRoutes
 ];
 
