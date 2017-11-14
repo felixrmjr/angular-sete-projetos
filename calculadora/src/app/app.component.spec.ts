22f44af6b6379424f60e23c9093d7910656d8196
Felix Ribeiro Mansor Junior <felix.mansor@superdigital.com.br>
2017-11-14 13:45:13 -0200

    Versionando o final do projeto da calculadora

--- calculadora/src/app/app.component.spec.ts
+++ calculadora/src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora/';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent        
       ],
+      imports: [
+        CalculadoraModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
